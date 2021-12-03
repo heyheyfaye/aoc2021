74dfe7ea989a7ff826658cdf951dd66f402d10be
heyheyfaye <36482766+heyheyfaye@users.noreply.github.com>
2021-12-03 18:10:28 +0100

    day2

---
+++ day2_part2.py
@@ -0,0 +1,28 @@
+import re 
+
+with open("puzzle2.txt", "r") as inputPuzzle2:
+    liste = inputPuzzle2.read().split("\n")
+
+#print(liste)
+
+horizontal = 0
+depth = 0
+aim = 0
+
+for value in liste:
+    if re.search('forward', value):
+        number = re.search('[0-9]', value).group(0)
+        horizontal+= int(number) 
+        depth += (aim*int(number))
+    elif re.search('up', value):
+        number = re.search('[0-9]', value).group(0)
+        #depth-= int(number)
+        aim -= int(number)
+    elif re.search('down', value):
+        number = re.search('[0-9]', value).group(0)
+        #depth+= int(number)
+        aim += int(number)
+    else:
+        print("where are you going?")
+
+print("final horizontal:", horizontal, "final depth:", depth, "answer:", horizontal*depth)
